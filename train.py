99fc9c4ac1aa5d6f9d0a8610d1c767f847b72d0d
Seongha Hwang <sseongha11@gmail.com>
2024-01-24 09:34:15 +0000

    Adding argument parser

--- train.py
+++ train.py
@@ -1,10 +1,14 @@
 # USAGE
-# python train.py
+# python train.py --model unet --output output/unet/
+# python train.py --model unet++ --output output/unetpp/
+# python train.py --model deeplabv3+ --output output/deeplabv3p/
+# python train.py --model fpn --output output/fpn/
 
 import os
 from matplotlib import pyplot as plt
 import pandas as pd
 import torch
+import argparse
 import segmentation_models_pytorch as smp
 import segmentation_models_pytorch.utils as su
 
@@ -15,120 +19,133 @@ from raac.dataset import CracksDataset
 from raac.helper_functions import get_validation_augmentation, get_preprocessing, get_training_augmentation
 
 
-def main():
-    # create segmentation model with pretrained encoder
-    model = smp.DeepLabV3Plus(
-        encoder_name=config.ENCODER,
-        encoder_weights=config.ENCODER_WEIGHTS,
-        classes=len(config.CLASSES),
-        activation=config.ACTIVATION,
-    )
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--model", type=str, default="unet",
+                choices=["unet", "unet++", "deeplabv3+", "fpn"],
+                help="name of segmentation model to train")
+ap.add_argument("-o", "--output", required=True, help="path to the output directory")
+args = vars(ap.parse_args())
 
-    preprocessing_fn = smp.encoders.get_preprocessing_fn(config.ENCODER, config.ENCODER_WEIGHTS)
+if not os.path.exists(args["output"]):
+    os.makedirs(args["output"])
 
-    # Get train and val dataset instances
-    train_dataset = CracksDataset(
-        config.x_train_dir, config.y_train_dir,
-        augmentation=get_training_augmentation(),
-        preprocessing=get_preprocessing(preprocessing_fn),
-        class_rgb_values=config.class_rgb_values,
-    )
+MODELS = {
+    "unet": smp.Unet,
+    "unet++": smp.UnetPlusPlus,
+    "deeplabv3+": smp.DeepLabV3Plus,
+    "fpn": smp.FPN,
+}
 
-    valid_dataset = CracksDataset(
-        config.x_valid_dir, config.y_valid_dir,
-        augmentation=get_validation_augmentation(),
-        preprocessing=get_preprocessing(preprocessing_fn),
-        class_rgb_values=config.class_rgb_values,
-    )
+# create segmentation model with pretrained encoder
+model = MODELS[args["model"]](
+    encoder_name=config.ENCODER,
+    encoder_weights=config.ENCODER_WEIGHTS,
+    classes=len(config.CLASSES),
+    activation=config.ACTIVATION,
+)
 
-    # Get train and val data loaders
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=5)
-    valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=2)
+preprocessing_fn = smp.encoders.get_preprocessing_fn(config.ENCODER, config.ENCODER_WEIGHTS)
 
-    # define loss function
-    loss = config.LOSS
+# Get train and val dataset instances
+train_dataset = CracksDataset(
+    config.X_TRAIN_DIR, config.Y_TRAIN_DIR,
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    class_rgb_values=config.CLASS_RGB_VALUES,
+)
 
-    # define metrics
-    metrics = config.METRIC
+valid_dataset = CracksDataset(
+    config.X_VALID_DIR, config.Y_VALID_DIR,
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    class_rgb_values=config.CLASS_RGB_VALUES,
+)
 
-    # define optimizer
-    optimizer = torch.optim.Adam([
-        dict(params=model.parameters(), lr=config.INIT_LR),
-    ])
+# Get train and val data loaders
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=5)
+valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=2)
 
-    # define learning rate scheduler (not used in this NB)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-        optimizer, T_0=1, T_mult=2, eta_min=5e-5,
-    )
+# define loss function
+loss = config.LOSS
 
-    # # load best saved model checkpoint from previous commit (if present)
-    # if os.path.exists(config.OUTPUT_PATH):
-    #     model = torch.load(config.OUTPUT_PATH, map_location=config.DEVICE)
+# define metrics
+metrics = config.METRIC
 
-    train_epoch = su.train.TrainEpoch(
-        model,
-        loss=loss,
-        metrics=metrics,
-        optimizer=optimizer,
-        device=config.DEVICE,
-        verbose=True,
-    )
+# define optimizer
+optimizer = torch.optim.Adam([
+    dict(params=model.parameters(), lr=config.INIT_LR),
+])
 
-    valid_epoch = su.train.ValidEpoch(
-        model,
-        loss=loss,
-        metrics=metrics,
-        device=config.DEVICE,
-        verbose=True,
-    )
+# define learning rate scheduler (not used in this NB)
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+    optimizer, T_0=1, T_mult=2, eta_min=5e-5,
+)
 
-    best_iou_score = 0.0
-    train_logs_list, valid_logs_list = [], []
+# # load best saved model checkpoint from previous commit (if present)
+# if os.path.exists(args["output"]):
+#     model = torch.load(args["output"], map_location=config.DEVICE)
 
-    for e in tqdm(range(0, config.EPOCHS)):
-        # Perform training & validation
-        print('\nEpoch: {}'.format(e))
-        train_logs = train_epoch.run(train_loader)
-        valid_logs = valid_epoch.run(valid_loader)
-        train_logs_list.append(train_logs)
-        valid_logs_list.append(valid_logs)
-        # file_name = f'model_epoch_{i}.pth'
-        # torch.save(model, os.path.join('./output', file_name))
+train_epoch = su.train.TrainEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    optimizer=optimizer,
+    device=config.DEVICE,
+    verbose=True,
+)
 
-        file_name_best = f'best_model.pth'
-        # Save model if a better val IoU score is obtained
-        if best_iou_score < valid_logs['iou_score']:
-            best_iou_score = valid_logs['iou_score']
-            torch.save(model, os.path.join(config.OUTPUT_PATH, file_name_best))
-            print('Model saved!')
+valid_epoch = su.train.ValidEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    device=config.DEVICE,
+    verbose=True,
+)
 
-    print("Evaluation on Test Data: ")
-    train_logs_df = pd.DataFrame(train_logs_list)
-    valid_logs_df = pd.DataFrame(valid_logs_list)
-    train_logs_df.T.to_csv(os.path.join(config.OUTPUT_PATH, 'train_logs.csv'))
+best_iou_score = 0.0
+train_logs_list, valid_logs_list = [], []
 
-    plt.figure(figsize=(20, 8))
-    plt.plot(train_logs_df.index.tolist(), train_logs_df.iou_score.tolist(), lw=3, label='Train')
-    plt.plot(valid_logs_df.index.tolist(), valid_logs_df.iou_score.tolist(), lw=3, label='Valid')
-    plt.xlabel('Epochs', fontsize=20)
-    plt.ylabel('IoU Score', fontsize=20)
-    plt.title('IoU Score Plot', fontsize=20)
-    plt.legend(loc='best', fontsize=16)
-    plt.grid()
-    plt.savefig(os.path.join(config.OUTPUT_PATH, 'iou_score_plot.png'))
-    plt.show()
+for e in tqdm(range(0, config.EPOCHS)):
+    # Perform training & validation
+    print('\nEpoch: {}'.format(e))
+    train_logs = train_epoch.run(train_loader)
+    valid_logs = valid_epoch.run(valid_loader)
+    train_logs_list.append(train_logs)
+    valid_logs_list.append(valid_logs)
+    # file_name = f'model_epoch_{i}.pth'
+    # torch.save(model, os.path.join(args["output"], file_name))
 
-    plt.figure(figsize=(20, 8))
-    plt.plot(train_logs_df.index.tolist(), train_logs_df.dice_loss.tolist(), lw=3, label='Train')
-    plt.plot(valid_logs_df.index.tolist(), valid_logs_df.dice_loss.tolist(), lw=3, label='Valid')
-    plt.xlabel('Epochs', fontsize=20)
-    plt.ylabel('Dice Loss', fontsize=20)
-    plt.title('Dice Loss Plot', fontsize=20)
-    plt.legend(loc='best', fontsize=16)
-    plt.grid()
-    plt.savefig(os.path.join(config.OUTPUT_PATH, 'dice_loss_plot.png'))
-    plt.show()
+    file_name_best = f'best_model.pth'
+    # Save model if a better val IoU score is obtained
+    if best_iou_score < valid_logs['iou_score']:
+        best_iou_score = valid_logs['iou_score']
+        torch.save(model, os.path.join(args["output"], file_name_best))
+        print('Model saved!')
 
+print("Evaluation on Test Data: ")
+train_logs_df = pd.DataFrame(train_logs_list)
+valid_logs_df = pd.DataFrame(valid_logs_list)
+train_logs_df.T.to_csv(os.path.join(args["output"], 'train_logs.csv'))
 
-if __name__ == '__main__':
-    main()
+plt.figure(figsize=(20, 8))
+plt.plot(train_logs_df.index.tolist(), train_logs_df.iou_score.tolist(), lw=3, label='Train')
+plt.plot(valid_logs_df.index.tolist(), valid_logs_df.iou_score.tolist(), lw=3, label='Valid')
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('IoU Score', fontsize=20)
+plt.title('IoU Score Plot', fontsize=20)
+plt.legend(loc='best', fontsize=16)
+plt.grid()
+plt.savefig(os.path.join(args["output"], 'iou_score_plot.png'))
+plt.show()
+
+plt.figure(figsize=(20, 8))
+plt.plot(train_logs_df.index.tolist(), train_logs_df.dice_loss.tolist(), lw=3, label='Train')
+plt.plot(valid_logs_df.index.tolist(), valid_logs_df.dice_loss.tolist(), lw=3, label='Valid')
+plt.xlabel('Epochs', fontsize=20)
+plt.ylabel('Dice Loss', fontsize=20)
+plt.title('Dice Loss Plot', fontsize=20)
+plt.legend(loc='best', fontsize=16)
+plt.grid()
+plt.savefig(os.path.join(args["output"], 'dice_loss_plot.png'))
+plt.show()
